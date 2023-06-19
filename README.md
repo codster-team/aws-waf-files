cb81ee18b43ca53ba80f6cb457f541738d0e0f0b
Gerardo Velázquez <gerardo@codster.io>
2023-06-19 17:53:54 -0600

    Initial upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS WAF Operations Dashboards for MultiAccount
+
+In this repository the files should public available to be reference from internal projects.
---
+++ create_esconfig.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Copyright Amazon.com, Inc. and its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License. See the LICENSE accompanying this file
+# for the specific language governing permissions and limitations under
+# the License.
+#
+
+if [ -z "$1" ]
+then
+    echo "Usage: $0 <bucket_name>"
+    exit 1
+fi
+pip3 install --user virtualenv
+virtualenv -p python3.7 venv
+source ./venv/bin/activate
+pip3 install -U requests requests-AWS4Auth cfnresponse
+pip3 install urllib3==2.0.2
+cd $VIRTUAL_ENV/lib/python3.7/site-packages/
+rm -rf wheel* easy_install* setuptools* _virtualenv* pip* _distutils* pkg_resources
+wget https://raw.githubusercontent.com/codster-team/aws-waf-files/lambda_function.py
+zip -r esconfig.zip .
+aws s3 cp esconfig.zip s3://$1
+deactivate
+cd ../../../..
+rm -rf venv build

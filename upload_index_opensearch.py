14a7a0d0927dcf36b7bf870e38fdab314b3e07fb
Gerardo Velázquez <gerardo@codster.io>
2023-06-20 22:00:44 -0600

    Adding script for uploading index template to opensearch

---
+++ upload_index_opensearch.py
@@ -0,0 +1,29 @@
+import os
+import boto3
+import requests
+from requests_aws4auth import AWS4Auth
+
+region = 'us-east-1'
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+host = 'https://search-codster-waf-dashboard-fxjhlkvko4n5loalxfaekzacvu.us-east-1.es.amazonaws.com'
+index = 'waf_logs'  # El nombre de tu índice
+url = host + '/_index_template/' + index 
+
+headers = { "Content-Type": "application/json" }
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    files_to_download = ['awswaf-es-index-template.json']
+
+    for file in files_to_download:
+        url_to_download = f'https://raw.githubusercontent.com/codster-team/aws-waf-files/main/{file}'
+        response = requests.get(url_to_download)
+        document = response.text
+        # Ahora subir el archivo a OpenSearch
+        r = requests.post(url, auth=awsauth, json=document, headers=headers)
+        print(r.text)
+
